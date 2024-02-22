a3c6dd827fd60b562e7e2918faf3986f206be074
Ramy Awad <ramyaqt20@gmail.com>
2024-02-22 12:44:48 +0200

    added create expense route

--- backend/main.py
+++ backend/main.py
@@ -5,12 +5,29 @@ from models import Expense
 
 @app.route("/expenses", methods=["GET"])
 def get_expenses():
-        expenses = map(lambda x: x.to_json(), Expense.query.all())
-        return jsonify({"expenses: ", expenses})
+        expenses = list(map(lambda x: x.to_json(), Expense.query.all()))
+        return jsonify({"expenses: ": expenses})
 
+@app.route("/create_expense", methods=["POST"])
+def create_expense():
+        title = request.json.get("Title")
+        amount = request.json.get("Amount")
+        reason = request.json.get("Reason")
+        
+        if not title or not amount:
+                return jsonify({"Please enter a title and an amount! "})
+        
+        new_expense = Expense(title, amount, reason)
+        
+        try:
+                db.session.add(new_expense)
+                db.session.commit()
+                return jsonify({"new expense created": new_expense})
+        except Exception as e:
+                return jsonify({"error encountered": e})
 
 if __name__ == "__main__":
         with app.app_context():
                 db.create_all()
                 
-        app.run()
+        app.run(debug=True)
